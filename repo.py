b298cdec9f87e98c11b31bfcd2eff1f740cdbcb2
Qureeb Hameed <quareebs@yahoo.com>
2020-02-04 12:10:52 +0200

    introduced pydriller. can now checkout and read files.

--- repo.py
+++ repo.py
@@ -1,15 +1,26 @@
-import os, git
+import os, git, pydriller
 currentDir = os.getcwd()
 
 gitHubRepo = "https://github.com/qureebme/fullStackOpen3.git"
 gitDir = gitHubRepo.rsplit('/', 1)[1].rsplit('.')[0] # extract dir name from url
-#print(gitDir) # name of the local git dir
 
 assert os.path.exists(gitDir) is False # to ensure the dir doesnt already exist
-
 git.Git(currentDir).clone(gitHubRepo) # clone the repo
 
-repo = git.Repo(gitDir)
-assert repo.__class__ is git.Repo
-assert not repo.bare
-assert not repo.is_dirty()  # check if repo is dirty
+repo_ = git.Repo(gitDir)
+assert repo_.__class__ is git.Repo
+assert not repo_.bare
+assert not repo_.is_dirty()  # check if repo is dirty
+
+repo = pydriller.GitRepository(gitDir) # cloned repo object
+
+for commit in pydriller.RepositoryMining(gitDir).traverse_commits():
+    hashh = commit.hash
+    mssg = commit.msg
+
+    repo.checkout(hashh)
+    files = repo.files()
+    
+    for file in files:
+        f = open(file, 'r')
+        f.close()
