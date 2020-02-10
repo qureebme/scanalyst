aab51e883b032900abc6ac04918322d5257c42ba
Henrik Sillanpää <sillan44@linux-desktop6.tuni.fi>
2020-02-10 20:13:13 +0200

    Added function

--- sonar_results.py
+++ sonar_results.py
@@ -1,27 +1,40 @@
-list1 = []
+from repo import checkRepo
 
 
-#f= open("text.txt,"r")
+def making_dict():
+
+    list_repo = checkRepo()
+
+    L= [None]*15
+
+
+    L.insert(0,list_repo[0][0]) # projectName
+    L.insert(1,list_repo[0][9]) # commiterdate / Creation date?
+    L.insert(2,list_repo[0][1]) # commit_hash
+
+
+    L.insert(5,list_repo[0][2]) # component
+
+    L.insert(11,list_repo[0][4]) # Message
+    L.insert(14,list_repo[0][3]) # author
 
 
 
-def making_dict(list1):
-    list1 = ["commons-daemon", "2020-09", "d3421394", "CODE_SMELL", "etc", "etc", "etc..."]
-    something_more = ["CODE_SMELL", "41414", "NOT IMPORTANT", "HEHE"]
     list_keys = ["projectName", "creationDate", "creationCommitHash", "type", "squid", "component", "severity"
                                                                                                     "startLine",
                  "endLine", "resolution", "status", "message", "effort", "debt", "author"]
-    something_str = ["number", "age", "id", "something"]
+
     # Create a zip object from two lists
-    zipbObj = zip(list_keys, list1)
+    zipbObj = zip(list_keys, L)
     # Create a dictionary from zip object
     dictOfWords = dict(zipbObj)
     # Create a zip object from two lists
-    zipbObj = zip(list_keys, list1)
+    zipbObj = zip(list_keys, L)
 
     # Create a dictionary from zip object
     dictOfWords = dict(zipbObj)
-    #print(dictOfWords)
+    
     return dictOfWords
 
-making_dict(list1)
+
+
