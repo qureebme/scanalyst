e25d0c3042b56b4692a5d56c09cd3790fcf3fdd3
KubbyDev <gbrljorge@gmail.com>
2020-02-13 15:17:14 +0200

    Python modules installer

--- install.sh
+++ install.sh
@@ -1,21 +1,37 @@
 #!/usr/bin/env bash
 
-SERVER_VERSION=8.1.0.31237
-SERVER_PATH=SonarQube
-SCANNER_VERSION=4.2.0.1873-linux
-SCANNER_PATH=SonarQube
+# Installs the whole program
 
-# TODO Verify prerequisits:
-# JDK >= 11     (run "java --version" to check)
-# Python version ?
+# Checks if Python >= 3.0 is installed
+echo "Checking Python version (>= 3.0 is needed)..."
+PYV=`python3 -c '\
+import sys;\
+import platform;\
+major, minor, patch = platform.python_version_tuple();\
+sys.stdout.write("OK" if (int(major) >= 3 and int(minor) >= 0) else "");\
+'`
+# If the above command couldn't execute then PYV = ""
+# If it executed but the version is less than 3.0 then PYV = ""
+if [[ -z "$PYV" ]]
+then
+    echo "ERROR: Please install Python >= 3.0"
+    exit 1
+fi
 
-# Installs sonarqube server
-wget -P $SERVER_PATH/ https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-$SERVER_VERSION.zip
-unzip $SERVER_PATH/sonarqube-$SERVER_VERSION.zip -d $SERVER_PATH/
-rm $SERVER_PATH/sonarqube-$SERVER_VERSION.zip 
+# Checks if pip3 is installed
+echo "Checking PIP3 installation..."
+PIPV=`python -m pip --version`
+if [[ -z "$PIPV" ]]
+then
+    echo "ERROR: Please install PIP3 (Python3 package installer)"
+    exit
+fi
 
-# Installs sonar scanner
-wget -P $SCANNER_PATH https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SCANNER_VERSION.zip
-unzip $SCANNER_PATH/sonar-scanner-cli-$SCANNER_VERSION.zip -d $SCANNER_PATH/
-rm $SCANNER_PATH/sonar-scanner-cli-$SCANNER_VERSION.zip
+# Installs needed python modules
+echo "Installing needed Python modules..."
+pip3 install gitpython
+pip3 install PyDriller
 
+# Installs sonarqube and sonar-scanner
+echo "Installing SonarQube and Sonar Scanner..."
+(cd SonarQube && ./install.sh)
