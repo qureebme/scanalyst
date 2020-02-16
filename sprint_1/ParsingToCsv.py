d60d0a2ef73226646ac16b54eec2dc582f43a218
alexis <alexis.vanhalle@tuni.fi>
2020-02-16 15:20:30 +0200

    correcting syntax error in ParsingCsv + moving all files in sprint1 folder

--- sprint_1/ParsingToCsv.py
+++ sprint_1/ParsingToCsv.py
@@ -2,7 +2,7 @@ import csv
 
 def make_csv(dicos,name="DefaultName",headers=None):
     with open (name+".csv", 'w', newline="") as csvfile:
-        writer=csv.DictWriter(csvfile,filednames=headers if headers else dicos[0].keys())
+        writer=csv.DictWriter(csvfile,fieldnames=headers if headers else dicos[0].keys())
 
         writer.writeheader()
         for i in dicos:
