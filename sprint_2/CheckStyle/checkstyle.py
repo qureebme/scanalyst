93c0427c5d50315fbca6b5fffe4d1e4bfe562d42
KubbyDev <gbrljorge@gmail.com>
2020-03-14 17:31:18 +0200

    Basic CheckStyle results parsing

--- sprint_2/CheckStyle/checkstyle.py
+++ sprint_2/CheckStyle/checkstyle.py
@@ -1,13 +1,47 @@
 import os
+import xml.etree.ElementTree as xml
 
 # Installation directory of sonar-scanner
 file = open("CheckStyle/checkstyle-jar-location", "r")
 __checkstyle_jar = file.read().replace("\n", "")
 file.close()
 
-# Analyses the given file
-def analyse(inputPath, outputPath):
+__results_file = "CheckStyle/res";
+
+# Runs the analysis on the given folder
+# CheckStyle will then store the results as xml in outputPath
+def __run_analysis(inputPath, outputPath):
     files = os.path.join(inputPath, "*.java") + " " + os.path.join(inputPath, "**/*.java")
-    output = "-o " + outputPath
+    output = "-o " + outputPath + " -f xml"
     config = "-c /sun_checks.xml"
     os.system("java -jar " + __checkstyle_jar +" "+ config +" "+ files +" "+ output)
+
+# Parses the xml to a dictionnary with the useful information
+def __parse_results(xmlFile):
+    res = []
+    root = xml.parse(xmlFile).getroot()
+    for f in root.findall("file"):
+        for error in f.findall("error"):
+            dic = {
+                "rule":"",
+                "severity": error.get("severity"),
+                "status":"",
+                "message": error.get("message"),
+                "effort":"",
+                "debt":"",
+                "type":"",
+                "creationDate":"",
+                "startLine": error.get("line"),
+                "endLine": error.get("line"),
+                "resolution":"null"
+            }
+            res.append(dic)
+
+    return res
+
+# Analyses the given folder and returns the results as a dictionnary
+def analyse(path):
+    __run_analysis(path, __results_file)
+    results = __parse_results(__results_file)
+    os.remove(__results_file)
+    return results
