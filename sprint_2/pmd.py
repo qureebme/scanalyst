46becb50cdf5438d24657d36021a03e6eae8eb03
Qureeb Hameed <quareebs@yahoo.com>
2020-03-15 11:49:35 +0200

    java code analysis with PMD

---
+++ sprint_2/pmd.py
@@ -0,0 +1,44 @@
+# The function takes a directory (e.g. /g12_1) as an argument.
+# Its output is a dict object about the PMD code analysis
+# of the java files in its 'main' directory (e.g. g12_1/main)
+
+# The commented lines of code are for possible modifications,
+# if needed later
+
+import os, subprocess
+
+def usePMD(code_dir):
+
+    if not os.path.isdir(code_dir):
+        raise Exception(code_dir + ' is not a directory')
+    
+    os.chdir(code_dir)
+
+    #for folder in code_dir, run analysis
+    #for dir in os.listdir('./'): # comment
+
+        #os.chdir(dir)   # comment
+
+    #indent_start
+    cmd1 = 'pmd -d ' + os.getcwd() + '/src/main/ -R rulesets/java/quickstart.xml -f csv -no-cache'    #default rulesets
+    res = subprocess.Popen(cmd1, shell=True, stdout=subprocess.PIPE).communicate()[0]
+
+    res2 = res.decode('utf-8').split('\n') # \r\n on Windows
+
+    fields = res2[0].split(',')
+    values = res2[1].split(',')
+
+    dict_res = dict(zip(fields, values))
+    dict_res.pop('"Problem"')
+
+    os.chdir('../')
+    #indent_end
+
+        #break
+
+    #os.chdir('../') # comment
+
+    return dict_res
+
+#usePMD('./code')
+#print(usePMD('./java-project2017'))
