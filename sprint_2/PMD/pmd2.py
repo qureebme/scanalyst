bf6299ab9b6dde6ed693bfe357f90353fb2a5d51
Qureeb Hameed <quareebs@yahoo.com>
2020-03-19 15:28:31 +0200

    restored for normal use

--- sprint_2/PMD/pmd2.py
+++ sprint_2/PMD/pmd2.py
@@ -1,19 +1,15 @@
-# The function takes a directory (e.g. /g12_1) as an argument.
+# The main function (usePMD) takes a directory (e.g. /g12_1) as an argument.
 # Its output is a list of dictionaries about the PMD code analysis
-# of the java files in its 'main' directory (e.g. g12_1/main)
-
-# The commented lines of code are for possible modifications,
-# if needed later
+# of the java files in its 'main' directory (e.g. g12_1/main).
+# Each dictionary contains details about one code issue.
 
 import os, subprocess
-'''
+
 # Reads the installation directory of pmd
 file = open("PMD/pmd-bin-location", "r")
 __pmdBinDir = file.read().replace("\n", "")
 file.close()
-'''
 
-#def usePMD(code_dir,pickUpMetaDataFun,output_data,commit_data):
 def usePMD(code_dir):
 
     if not os.path.isdir(code_dir):
@@ -21,14 +17,7 @@ def usePMD(code_dir):
 
     os.chdir(code_dir)
 
-    #for folder in code_dir, run analysis
-    #for dir in os.listdir('./'): # comment
-
-        #os.chdir(dir)   # comment
-
-    #indent_start
-    #cmd1 = os.path.join(__pmdBinDir, "run.sh ") + 'pmd -d ' + os.getcwd() + '/src/main/ -R rulesets/java/quickstart.xml -f csv -no-cache'    #default rulesets
-    cmd1 = 'pmd -d ' + os.getcwd() + '/src/main/ -R rulesets/java/quickstart.xml -f csv -no-cache'    #default rulesets
+    cmd1 = os.path.join(__pmdBinDir, "run.sh ") + 'pmd -d ' + os.getcwd() + '/src/main/ -R rulesets/java/quickstart.xml -f csv -no-cache'    #default rulesets
 
     output = subprocess.Popen(cmd1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     res = output[0]
@@ -37,21 +26,18 @@ def usePMD(code_dir):
         print('PMD: something went wrong: ', err)
         return
 
-    res2 = res.decode('utf-8').split('\r\n') # all code issues
+    res2 = res.decode('utf-8').split('\n') # all code issues
     res2.pop(0)
 
-    #print(res2)
     fields = ['component', 'severity', 'startLine', 'resolution', 'type','rule'] # best fit
     values = processIssues(res2)
-    #print(values)
 
     dictList = list()
 
-    
     for item in values:
         fullDict = dict()
+
         for index in range(0,6):
-            
             fullDict[fields[index]] = item[index]
         dictList.append(fullDict)
 
@@ -79,7 +65,6 @@ def cleanUpEntry(entry):
         return entry
     else:
         return entry
-
 
 #usePMD('./code')
 #print(usePMD('./java-project2017'))
