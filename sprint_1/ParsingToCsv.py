0260745f9b9cd09f61290f6b5a57a1357c80e894
qureeb hameed <quareebs@yahoo.com>
2020-04-21 22:40:05 +0300

    modified SonarQube permission

--- sprint_1/ParsingToCsv.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def make_csv(dicos,name="DefaultName",headers=None):
-    with open (name+".csv", 'w', newline="") as csvfile:
-        writer=csv.DictWriter(csvfile,fieldnames=headers if headers else dicos[0].keys())
-
-        writer.writeheader()
-        for i in dicos:
-            writer.writerow(i)
